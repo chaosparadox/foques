42d2fce842ae182abeb527aeb9bad9d22e271486
chaosparadox <108727862+chaosparadox@users.noreply.github.com>
2022-10-28 16:56:25 +0300

    build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,8 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 const index = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount(count + 1);
-  };
-  const decreaseCount = () => {
-    setCount(count - 1);
-  };
-  const resetCount = () => {
-    setCount(0);
-  };
-  return (
-    <div className="p-8 flex flex-col justify-center items-center">
-      <div className="space-x-4 flex">
-        <button className=" bg-red-400 rounded-xl px-4" onClick={decreaseCount}>
-          -
-        </button>
-        <div>{count}</div>
-        <button
-          className=" bg-primary-200 rounded-xl px-4"
-          onClick={increaseCount}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <button onClick={resetCount}>Reset</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default index;
